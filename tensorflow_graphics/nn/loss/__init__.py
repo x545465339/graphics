c0711e931bc5ab5a38cec9bd60a9b609869bc66d
segaviv <segaviv@google.com>
2020-02-12 08:55:04 -0800

    Add a loss module to tensorflow_graphics.nn with an implementation of the Chamfer distance.
    PiperOrigin-RevId: 294678219

--- tensorflow_graphics/nn/loss/__init__.py
+++ tensorflow_graphics/nn/loss/__init__.py
@@ -11,14 +11,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Neural Network module."""
+"""Loss module."""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from tensorflow_graphics.nn import layer
-from tensorflow_graphics.nn import loss
+from tensorflow_graphics.nn.loss import chamfer
 from tensorflow_graphics.util import export_api as _export_api
 
-# API contains submodules of tensorflow_graphics.nn.
+# API contains submodules of tensorflow_graphics.nn.loss.
 __all__ = _export_api.get_modules()
